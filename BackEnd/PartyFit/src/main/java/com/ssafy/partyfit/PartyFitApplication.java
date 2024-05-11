dab2c329e442c919ce75f6f6c8d2df31f6a77347
jiyoung-Heo <jiyoungHeo.dev@gmail.com>
2024-05-12 08:48:56 +0900

    initial commit

---
+++ BackEnd/PartyFit/src/main/java/com/ssafy/partyfit/PartyFitApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.partyfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartyFitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartyFitApplication.class, args);
+	}
+
+}
---
+++ BackEnd/PartyFit/src/test/java/com/ssafy/partyfit/PartyFitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.partyfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartyFitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
