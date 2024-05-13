bb85939c84690ac3ea058715413815d0cfd5ca28
jiyoung-Heo <jiyoungHeo.dev@gmail.com>
2024-05-13 21:02:41 +0900

    Fix(showParty): Fix showparty RequestBody to ModelAttribute

--- BackEnd/PartyFit/src/main/java/com/ssafy/partyfit/controller/PartyController.java
+++ BackEnd/PartyFit/src/main/java/com/ssafy/partyfit/controller/PartyController.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,7 +23,7 @@ public class PartyController {
 	}
 	
 	@GetMapping("/show")
-	public ResponseEntity<?> showParty(@RequestBody SearchCondition condition){
+	public ResponseEntity<?> showParty(@ModelAttribute SearchCondition condition){
 		List<Party> partyList = partyService.showParty(condition);
 		if(partyList == null || partyList.size() == 0) {
 			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
