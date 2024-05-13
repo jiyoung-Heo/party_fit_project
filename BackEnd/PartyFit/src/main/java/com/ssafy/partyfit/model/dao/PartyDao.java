ccb26e954c2b14ce8e4f73d13de89ab5dec8328a
jiyoung-Heo <jiyoungHeo.dev@gmail.com>
2024-05-13 17:54:06 +0900

    Feat(showParty): Add showParty method

---
+++ BackEnd/PartyFit/src/main/java/com/ssafy/partyfit/model/dao/PartyDao.java
@@ -0,0 +1,10 @@
+package com.ssafy.partyfit.model.dao;
+
+import java.util.List;
+
+import com.ssafy.partyfit.model.dto.SearchCondition;
+import com.ssafy.partyfit.model.dto.Party;
+
+public interface PartyDao {
+	List<Party> selectAllParty(SearchCondition condition);
+}
---
+++ BackEnd/PartyFit/src/main/java/com/ssafy/partyfit/model/service/PartyService.java
@@ -0,0 +1,10 @@
+package com.ssafy.partyfit.model.service;
+
+import java.util.List;
+
+import com.ssafy.partyfit.model.dto.Party;
+import com.ssafy.partyfit.model.dto.SearchCondition;
+
+public interface PartyService {
+	public List<Party> showParty(SearchCondition condition);
+}
