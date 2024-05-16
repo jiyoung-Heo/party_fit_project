17e6526c769c177f5169493dd7940a26997f71ea
jiyoung-Heo <jiyoungHeo.dev@gmail.com>
2024-05-16 17:56:42 +0900

    Feat(showJoinList,clickArticleLikes): Add 관리자 가입 요청 조회, 승인, 거절

--- BackEnd/PartyFit/src/main/java/com/ssafy/partyfit/model/dao/PartyMemberDao.java
+++ BackEnd/PartyFit/src/main/java/com/ssafy/partyfit/model/dao/PartyMemberDao.java
@@ -3,13 +3,14 @@ package com.ssafy.partyfit.model.dao;
 import java.util.List;
 import java.util.Map;
 
-import com.ssafy.partyfit.model.dto.Article;
 import com.ssafy.partyfit.model.dto.Party;
+import com.ssafy.partyfit.model.dto.PartyMember;
 import com.ssafy.partyfit.model.dto.PartyMemberUser;
 import com.ssafy.partyfit.model.dto.User;
 
 public interface PartyMemberDao {
 	List<PartyMemberUser> selectPartyMember(Map<String, Object> map);
 	List<Party> selectMyParty(User user);
-	
+	int updatePartyMember(PartyMember party);
+	int deletePartyMember(PartyMember party);
 }
--- BackEnd/PartyFit/src/main/java/com/ssafy/partyfit/model/service/PartyMemberService.java
+++ BackEnd/PartyFit/src/main/java/com/ssafy/partyfit/model/service/PartyMemberService.java
@@ -5,10 +5,12 @@ import java.util.Map;
 
 import com.ssafy.partyfit.model.dto.Article;
 import com.ssafy.partyfit.model.dto.Party;
+import com.ssafy.partyfit.model.dto.PartyMember;
 import com.ssafy.partyfit.model.dto.PartyMemberUser;
 import com.ssafy.partyfit.model.dto.User;
 
 public interface PartyMemberService {
 	List<PartyMemberUser> showPartyMember(Map<String, Object> map);
 	public List<Party> showMyParty(User user);
+	public int managePartyMember(PartyMember partyMember, boolean isAccept);
 }
