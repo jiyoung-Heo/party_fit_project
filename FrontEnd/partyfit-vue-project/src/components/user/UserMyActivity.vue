da9e41064a9317515caf7703d92457611ba24f24
DayoungLee <daylee4564@naver.com>
2024-05-18 19:36:15 +0900

    Feat: Add 나의 참여 탭

---
+++ FrontEnd/partyfit-vue-project/src/components/user/UserMyActivity.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+    나의 참여내역 탭
+
+</div>
+</template>
+
+
+<script setup>
+import { useUserStore } from "@/stores/user";
+import { ref, onMounted, watch } from "vue";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+const store = useUserStore();
+const loginUser = ref()
+
+onMounted(() => {
+})
+
+
+</script>
+
+
+<style scoped>
+</style>
