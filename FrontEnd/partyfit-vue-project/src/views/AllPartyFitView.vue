31a6f4950f212fb55bc8ff75344b71d644caaad1
DayoungLee <daylee4564@naver.com>
2024-05-20 15:10:45 +0900

    Feat: Add 파티 누르면 연결

--- FrontEnd/partyfit-vue-project/src/views/AllPartyFitView.vue
+++ FrontEnd/partyfit-vue-project/src/views/AllPartyFitView.vue
@@ -1,10 +1,13 @@
 <template>
     <div class="container">
         전체 partyfit 클릭 시 나오는 페이지입니다.
+        <TheBestPartyFit />
     </div>
 </template>
 
 <script setup>
+import TheBestPartyFit from '@/components/partyfit/TheBestPartyFit.vue';
+
 
 </script>
 
