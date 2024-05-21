45ce3561bb52518574b903b06150ae3072c4a2cb
jiyoung-Heo <jiyoungHeo.dev@gmail.com>
2024-05-21 13:49:32 +0900

    Feat(UserDelete): Add 회원 탈퇴

--- FrontEnd/partyfit-vue-project/src/components/user/UserDelete.vue
+++ FrontEnd/partyfit-vue-project/src/components/user/UserDelete.vue
@@ -1,26 +1,57 @@
 <template>
-    <div>
-        회원탈퇴 탭
-    
-    </div>
-    </template>
+  <div>
+    <label for="changePW">현재 비밀번호</label>
+    <input type="password" v-model="nowPW" placeholder="현재 비밀번호" />
+    <div v-if="!isSame">비밀번호가 달라요</div>
+    <button @click="deleteUser">탈퇴하기</button>
+  </div>
+</template>
     
     
     <script setup>
-    import { useUserStore } from "@/stores/user";
-    import { ref, onMounted, watch } from "vue";
-    import { useRouter } from "vue-router";
-    
-    const router = useRouter();
-    const store = useUserStore();
-    const loginUser = ref()
-    
-    onMounted(() => {
-    })
-    
-    
-    </script>
+import { useUserStore } from "@/stores/user";
+import { ref, onMounted, watch } from "vue";
+import { useRouter } from "vue-router";
+import Swal from "sweetalert2";
+
+const router = useRouter();
+const store = useUserStore();
+const isSame = ref(true);
+
+onMounted(() => {});
+const nowPW = ref();
+const deleteUser = () => {
+  if (nowPW.value == store.loginUser.password) {
+    Swal.fire({
+      title: "탈퇴하시겠습니까?",
+      text: "되돌릴 수 없습니다",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      cancelButtonText: "취소",
+      confirmButtonText: "탈퇴",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        store.userLeaveRequest();
+        Swal.fire({
+          title: "탈퇴완료!",
+          text: "탈퇴되었습니다. 메인페이지로 이동합니다.",
+          icon: "success",
+        }).then(()=>{
+            console.log("test")
+            //회원탈퇴진행
+            store.$reset();
+            router.push({name:'home'})
+        });
+      }
+    });
+  } else {
+    isSame.value = false;
+  }
+};
+</script>
     
     
     <style scoped>
-    </style>
+</style>
--- FrontEnd/partyfit-vue-project/src/components/user/UserDetail.vue
+++ FrontEnd/partyfit-vue-project/src/components/user/UserDetail.vue
@@ -10,11 +10,6 @@
           alt="프로필이미지"
           class="position-relative" style="overflow: hidden"
         />
-        <span class="position-absolute bottom-0 start-100 pencil-out-circle">
-          <span class="badge" @click="changeImage">
-            <svg class="icon" xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 -960 960 960" width="35px" fill="#d8d8d8"><path d="M186.67-186.67H235L680-631l-48.33-48.33-445 444.33v48.33ZM120-120v-142l559.33-558.33q9.34-9 21.5-14 12.17-5 25.5-5 12.67 0 25 5 12.34 5 22 14.33L821-772q10 9.67 14.5 22t4.5 24.67q0 12.66-4.83 25.16-4.84 12.5-14.17 21.84L262-120H120Zm652.67-606-46-46 46 46Zm-117 71-24-24.33L680-631l-24.33-24Z"/></svg>
-          </span>
-        </span>
       </div>
       </div>
     <fieldset>
@@ -22,10 +17,6 @@
         <label for="loginId">ID</label>
         {{ user.loginId }}
       </div>
-      <!-- <div> -->
-        <!-- <label for="password">PW</label> -->
-        <!-- <button @click="changePW">비밀번호 변경</button> -->
-      <!-- </div> -->
       <div>
         <label for="name">이름</label>
         {{ user.name }}
@@ -87,62 +78,11 @@ watch(loginUser, (newVal, oldVal) => {
 const changePW = function () {
   router.push({ name: "changePW" });
 };
-const changeImage = () => {
-  Swal.fire({
-    title: '프로필 이미지 변경',
-    html: `
-      <div style="display: flex; flex-direction: column; align-items: center;">
-       기존 <img src="src/assets/user/${store.loginUser.profile}" id="currentImage" style="margin-bottom: 10px; width: 100px; height: 100px; object-fit: cover; border-radius: 50%;" />
-        <input type="file" id="fileInput" accept="image/*" />
-       수정 후 <img id="previewImage" style="margin-top: 10px; width: 100px; height: 100px; object-fit: cover; border-radius: 50%;" />
-      </div>
-    `,
-    showCancelButton: true,
-    confirmButtonText: '변경',
-    cancelButtonText: '취소',
-    preConfirm: () => {
-      const file = Swal.getPopup().querySelector('#fileInput').files[0];
-      if (!file) {
-        Swal.showValidationMessage(`이미지를 선택해 주세요`);
-      } else {
-        return file;
-      }
-    }
-  }).then((result) => {
-    if (result.isConfirmed) {
-      const reader = new FileReader();
-      reader.onload = (e) => {
-        console.log(e.target)
-        // loginUser.value.profile = e.target.result;
-        store.updateUser(loginUser.value, result.value)
 
-};
-      reader.readAsDataURL(result.value);
-    }
-  });
-
-  const fileInput = document.getElementById('fileInput');
-  const previewImage = document.getElementById('previewImage');
-  fileInput.addEventListener('change', () => {
-    const file = fileInput.files[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.onload = (e) => {
-        previewImage.src = e.target.result;
-      };
-      reader.readAsDataURL(file);
-    }
-  });
-};
 </script>
 
 
 <style scoped>
-.pencil-out-circle{
-    border-radius: 50%;
-    background-color: white;
-    border: 1px solid #d8d8d8;
-}
 
 img {
   width: 200px;
